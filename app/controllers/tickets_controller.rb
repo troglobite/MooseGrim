d06abc9bd380222d0a02b2e7fe10bb6565cb3cc2
Brian Parry <brian.parry@acimacredit.com>
2021-02-03 19:33:09 -0700

    added create controller action and index view (#6)

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,4 +2,18 @@ class TicketsController < ApplicationController
   def new
     @ticket = Ticket.new
   end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+  end
+
+  def index
+    @tickets = Ticket.all
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:description, :pr_url, :ticket_url, :status, :notes)
+  end
 end
