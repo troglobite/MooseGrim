a4a542b5fac3f6b630751eee36432640816aab01
Brian Parry <brianparry@pop-os.localdomain>
2021-03-30 19:18:21 -0600

    Added format validation and updated tests

--- test/models/ticket_test.rb
+++ test/models/ticket_test.rb
@@ -9,9 +9,17 @@ class TicketTest < ActiveSupport::TestCase
 
   test "Cannot save ticket without a descripton" do
     invalid_ticket = Ticket.create(status: "invalid")
-    valid_ticket = Ticket.create(description: "valid")
+    valid_ticket = Ticket.create(description: "valid", pr_url: "http://isaurl.com")
 
-    assert !invalid_ticket.save, "Saved a ticket without a description"
-    assert valid_ticket.save, "Failed to save a ticket with only description"
+    assert !invalid_ticket.valid?, "Saved a ticket without a description"
+    assert valid_ticket.valid?, "Failed to save a ticket with only description"
+  end
+
+  test "Cannot save ticket without a valid pr_url" do
+    invalid_ticket = Ticket.create(description: "invalid", pr_url: "not-a-url")
+    valid_ticket = Ticket.create(description: "valid", pr_url: "http://isaurl.com")
+
+    assert !invalid_ticket.valid?, "Saved a ticket with a bad URL"
+    assert valid_ticket.valid?, "Failed to save a ticket a good URL"
   end
 end
