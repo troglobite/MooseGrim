e31237d48ed1ccaba57b35e85db9ec17a5a0b794
Brian Parry <brian.parry@acimacredit.com>
2021-04-07 18:55:42 -0600

    Brian p/update pr url validations (#9)
    * Added format validation and updated tests
    
    * Added validation errors to ticket#new view
    
    Co-authored-by: Brian Parry <brianparry@pop-os.localdomain>

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,7 @@
 class Ticket < ApplicationRecord
   validates :description, presence: true
+  validates_format_of :pr_url,
+    :with => /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,63}(:[0-9]{1,5})?(\/.*)?\z/ix,
+    allow_blank: true,
+    message: "not a valid URL"
 end
