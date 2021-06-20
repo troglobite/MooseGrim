8008029e26f63b0e9ba656a9e88cc4e03641afd1
Brian Parry <brianparry089@gmail.com>
2021-06-19 19:00:56 -0600

    Fixing rubocop concerns

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,12 @@
 // that code so it'll be compiled.
 //= require bootstrap
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
