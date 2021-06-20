d47a497e1a29d369e8503030625063091500c7a7
Brian Parry <brian.parry@acimacredit.com>
2021-06-19 19:38:24 -0600

    Redesign Tickets#new page (#20)

--- Gemfile
+++ Gemfile
@@ -21,8 +21,12 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-# Use slim as the HTML prepaser
+
+# Frontend technologies
 gem 'slim', '~> 4.1.0'
+gem 'bootstrap', '~> 5.0.1'
+gem 'bootstrap-icons', '~> 1.0', '>= 1.0.10'
+gem 'sass', '~> 3.4', '>= 3.4.22'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,5 +3,17 @@ class Ticket < ApplicationRecord
   validates_format_of :pr_url,
     :with => /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,63}(:[0-9]{1,5})?(\/.*)?\z/ix,
     allow_blank: true,
-    message: "not a valid URL"
+    message: 'not a valid URL'
+
+  STATUSES = [
+    'product_backlog',
+    'ready_for_development',
+    'in_development',
+    'blocked',
+    'in_review',
+    'in_qa',
+    'returned_to_developement',
+    'ready_for_deploy',
+    'deployed'
+].freeze
 end
