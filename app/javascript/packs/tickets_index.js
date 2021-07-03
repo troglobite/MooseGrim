bbf5db225d24abfcc0e62c331e83b2b40069c0e8
Brian Parry <brianparry089@gmail.com>
2021-07-03 10:13:35 -0600

    Finished fetch request from tickets index JS

--- app/javascript/packs/tickets_index.js
+++ app/javascript/packs/tickets_index.js
@@ -20,10 +20,10 @@ export function onload() {
 const newTicketForm = document.querySelector('.new-ticket-form');
 const newButton = document.querySelector('#new-ticket-button');
 const submitButton = document.querySelector('.new-ticket-submit');
-const pr_url_field = document.querySelector('.pr_url_field');
-const ticket_url_field = document.querySelector('.ticket_url_field');
-const status_field = document.querySelector('.status_field');
-const notes_field = document.querySelector('.notes_field');
+const pr_url_field = document.querySelector('#pr_url_field');
+const ticket_url_field = document.querySelector('#ticket_url_field');
+const status_field = document.querySelector('#status_field');
+const notes_field = document.querySelector('#notes_field');
 
 onload().then(toggleHidden(newTicketForm));
 
@@ -34,21 +34,24 @@ newButton.addEventListener('click', event => {
 
 submitButton.addEventListener('click', event => {
   event.preventDefault();
-  submitForm();
+  submitForm(event);
 });
 
-export function submitForm() {
+export function submitForm(event) {
   const data = {
-    pr_url: pr_url_field,
-    ticket_url: ticket_url_field,
-    status_field: status_field,
-    notes_field: notes_field
+    pr_url: pr_url_field.value,
+    ticket_url: ticket_url_field.value,
+    status_field: status_field.value,
+    notes_field: notes_field.value
   }
-  fetch('/tickets', {
+  fetch(event.target.dataset.ticketsUrl, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    headers: { 
+      'Content-Type': 'application/json', 
+      "Accept": "application/json",
+      "X-CSRF-Token" : document.getElementsByName("csrf-token")[0].content
+    },
     body: JSON.stringify(data)
   })
-    .then(response => response.json())
-    .then(data => console.log(data));
+  .then(response => console.log(response))
 }
