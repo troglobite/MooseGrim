26cf5ce0ac77977e7f55565711883e64875b9244
troglobite <brianparry089@gmail.com>
2021-12-07 21:45:27 -0700

    Adding crud actions to tickets controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -24,9 +24,27 @@ class TicketsController < ApplicationController
     @tickets = Ticket.all
   end
 
+  def edit
+    @ticket = Ticket.find(ticket_params[:id])
+  end
+
+  def update
+    @ticket = Ticket.find(ticket_params[:id])
+
+    @ticket.assign_attributes(ticket_params)
+
+    if @ticket.valid?
+      @ticket.save!
+      flash[:notice] = "New ticket saved sucessfully"
+      redirect_to tickets_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def ticket_params
-    params.require(:ticket).permit(:description, :pr_url, :ticket_url, :status, :notes)
+    params.require(:ticket).permit(:description, :pr_url, :ticket_url, :status, :notes, :id)
   end
 end
